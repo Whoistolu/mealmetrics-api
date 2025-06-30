16547cdbbaeb489b743ac56a27dd9238f77f5d4a
Whoistolu <digittolu25@gmail.com>
2025-06-30 15:20:34 +0100

    Add JWT authentication to User model and configured Devise initializer

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,13 @@ class User < ApplicationRecord
   belongs_to :organization
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable,
+        :registerable,
+         :recoverable,
+         :rememberable,
+         :validatable,
+         :jwt_authenticatable,
+         jwt_revocation_strategy: Devise::JWT::RevocationStrategies::Null
 
-  enum role: { admin: 'admin', manager: 'manager', staff: 'staff' }
+  enum role: { admin: "admin", manager: "manager", staff: "staff" }
 end

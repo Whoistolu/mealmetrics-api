d6a5528f65bbcf2ca131e2c02cd5f7130eac642e
Whoistolu <digittolu25@gmail.com>
2025-07-01 22:47:25 +0100

    Update Application Controller to set the organization of the Current User

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::API
+    before_action :authenticate_user!
+    before_action :set_current_organization
+
+
+    def set_current_organization
+      @current_organization = current_user.organization
+    end
 end

2ef721259daff9854599b8e8b75719f94884623d
Whoistolu <digittolu25@gmail.com>
2025-07-01 23:07:22 +0100

    Configure pundit. Set up a rescue if a user tries an action they’re not authorized to do

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,18 @@
 class ApplicationController < ActionController::API
+    include Pundit
     before_action :authenticate_user!
     before_action :set_current_organization
 
 
     def set_current_organization
       @current_organization = current_user.organization
+    end
+
+    rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+    private
+
+    def user_not_authorized(exception)
+        render json: { error: "You are not authorized to perform this action." }, status: :forbidden
     end
 end

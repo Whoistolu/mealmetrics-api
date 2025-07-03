3e2246c8868dff20e8a120a8a66a7b4c45e355cd
Whoistolu <digittolu25@gmail.com>
2025-07-03 23:59:54 +0100

    Refactor Employee policy file

--- app/policies/employee_policy.rb
+++ app/policies/employee_policy.rb
@@ -1,14 +1,13 @@
 class EmployeePolicy < ApplicationPolicy
-  # NOTE: Up to Pundit v2.3.1, the inheritance was declared as
-  # `Scope < Scope` rather than `Scope < ApplicationPolicy::Scope`.
-  # In most cases the behavior will be identical, but if updating existing
-  # code, beware of possible changes to the ancestors:
-  # https://gist.github.com/Burgestrand/4b4bc22f31c8a95c425fc0e30d7ef1f5
+  # 🔹 Controller actions
 
+  
+
+  # 🔹 Scope class for index queries
   class Scope < ApplicationPolicy::Scope
-    # NOTE: Be explicit about which records you allow access to!
-    # def resolve
-    #   scope.all
-    # end
+    def resolve
+      # Only allow employees for the user's organization
+      scope.joins(:restaurant).where(restaurants: { organization_id: user.organization_id })
+    end
   end
 end
